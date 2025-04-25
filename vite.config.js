9a84d87fba06b77b12c270102bf1aaf085bead47
purpurrya <anastasia.tikhonenko.meow@gmail.com>
2025-04-26 02:04:11 +1000

    fix conf

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    preserveSymlinks: true, 
-  }
+    preserveSymlinks: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
 });
