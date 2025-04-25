8c460802c41597de390872d91466d2d7ab59c8ba
purpurrya <anastasia.tikhonenko.meow@gmail.com>
2025-04-25 19:43:15 +1000

    fix for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './assets/pages/Home'; 
 import TableView from './assets/pages/TableView';  
 
